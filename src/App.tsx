b46d013514250ede9661c6c7fa6ec3350b58c2ff
Goda <goda.vaicyte@gmail.com>
2023-06-19 17:07:26 +0300

    monday progress: input

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import './App.css';
+import Input from './components/atoms/Input';
+import searchIcon from './assets/search-icon.svg';
+import { useState } from 'react';
+import Table from './components/atoms/Table';
 
 function App() {
-  return <>Hello World</>;
+  const [search, setSearch] = useState('');
+
+  return (
+    <main>
+      <section>
+        <Input
+          type='text'
+          value={search}
+          setValue={setSearch}
+          placeholder='Search'
+          icon={searchIcon}
+        />
+        <Table columns={4} rows={3}/>
+      </section>
+    </main>
+  );
 }
 
 export default App;
